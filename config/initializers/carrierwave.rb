95206583ba8721e0d7f5ab9bad38142b50ed3e83
moripp <msa.m16a.i212z4a@gmail.com>
2019-12-16 15:47:13 +0900

    add carrierwave.rb
    主にfogのアップロード先の設定を記述している。
    画像アップロード先をローカルと本番で条件分岐させている。

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,22 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:aws_access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:aws_secret_access_key],
+      region: 'ap-northeast-1'
+    }
+
+    config.fog_directory  = 'freemarket.sample.59b'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket.sample.59b'
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
+end
