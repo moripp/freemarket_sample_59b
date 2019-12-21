f56db4982c81121a05a19e80c7a13c753d25d44d
Kaori Oka <kaorioka09jm@gmail.com>
2019-12-21 18:55:09 +0900

    Commit progress
    担当箇所をトレードするため、途中経過をコミットします。

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def index2
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/index2'
   root "exam#index"
+  
   resources :items, only: [:new]
 end
