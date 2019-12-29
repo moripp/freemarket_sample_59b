8f5b2da187f79ea54ed7d707da8c0fb4978b0a68
moripp <msa.m16a.i212z4a@gmail.com>
2019-12-29 20:39:56 +0900

    add validates image
    imageテーブルのカラムにバリデーションをかける

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
-  has_many :images
+  has_many :images  
   accepts_nested_attributes_for :images # 商品の情報と一緒に画像も保存されるようにする
+  validates :images, associated: true
 
   validates :name, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length: { maximum: 1000 }
