58ddd017b90ec41d0144eefe9af54e6a73842c3f
Pirori3182 <pomuo.0331@gmail.com>
2019-12-30 17:13:40 +0900

    made "add credit card page"

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,2 +1,7 @@
 class CreditCardsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "exam#index"
   resources :items, only: [:new, :create]
   resources :profiles,only: [:edit,:index]
+  resources :credit_cards,only: [:index, :create]
 end
