1c8e9fca2455216e949df9014c87b3e8809655e0
moripp <msa.m16a.i212z4a@gmail.com>
2020-01-01 19:55:22 +0900

    add validate

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,5 @@ class Item < ApplicationRecord
   validates :name, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length: { maximum: 1000 }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  validates :status, inclusion: { in: %w(for_sale not_sale sold_out) } # statusカラムの中身を3種類に限定
 end
