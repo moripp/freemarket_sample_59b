5f16e0e71be86e16770c382c99b7f7048e537283
moripp <msa.m16a.i212z4a@gmail.com>
2020-01-01 19:55:07 +0900

    add default

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,9 @@
 class Item < ApplicationRecord
   has_many :images  
   accepts_nested_attributes_for :images # 商品の情報と一緒に画像も保存されるようにする
-  validates :images, associated: true
+  attribute :status, :string, default: 'for_sale' # statuカラムにはデフォルトでfor_saleを入力
 
+  validates :images, associated: true
   validates :name, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length: { maximum: 1000 }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
