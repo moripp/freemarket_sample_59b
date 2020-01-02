6ec890b41bdecda492b3e5622fd24f90d42e658b
Pirori3182 <pomuo.0331@gmail.com>
2020-01-02 23:27:06 +0900

    editアクションをnewアクションに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "exam#index"
   resources :items, only: [:new, :create]
-  resources :profiles,only: [:edit,:index]
+  resources :profiles,only: [:new,:index]
   resources :credit_cards,only: [:index, :create]
 end
