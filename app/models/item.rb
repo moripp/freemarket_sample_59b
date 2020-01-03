270e79423a6487ee748840ce17782c63d0a84f55
moripp <msa.m16a.i212z4a@gmail.com>
2020-01-03 19:24:32 +0900

    add unum
    enumを追加
    もともと定義していたvalidateも不要になりそうなので削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,10 @@ class Item < ApplicationRecord
   has_many :images  
   accepts_nested_attributes_for :images # 商品の情報と一緒に画像も保存されるようにする
 
+  enum status: {for_sale: 0, not_sale: 1}
 
   validates :images, associated: true
   validates :name, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length: { maximum: 1000 }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
-  validates :status, inclusion: { in: %w(for_sale not_sale sold_out) } # statusカラムの中身を3種類に限定
 end
