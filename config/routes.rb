45a2b811046b4fb24378ea069866f4dd25a19179
moripp <msa.m16a.i212z4a@gmail.com>
2020-01-05 16:31:37 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "home#index"
   resources :items, only: [:new, :create, :show]
   resources :profiles,only: [:new,:edit,:index]
   resources :credit_cards,only: [:index,:new,:create]
-  devise_for :users
 end
