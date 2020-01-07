e67847649471e0b7cff392e0c5a63ae7def8e2e7
Kaori Oka <kaorioka09jm@gmail.com>
2020-01-08 01:12:42 +0900

    edit routs

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,16 @@ Rails.application.routes.draw do
   resources :items, only: [:new, :create]
   resources :profiles,only: [:new,:index]
   resources :credit_cards,only: [:index, :new]
+
+  # ウィザード形式の記述
+  resources :signup do
+    collection do
+      get 'step1'
+      get 'step2'
+      get 'step3'
+      get 'step4' # 入力最終ステップ
+      get 'done' # 登録完了後のページ
+    end
+  end
+  
 end
