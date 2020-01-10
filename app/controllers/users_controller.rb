ccf2a373e6348b22e07a3b2d7ef2f57c8a8a12e1
hama-chan-tech-camp <g13tomo@docomo.ne.jp>
2020-01-10 20:02:03 +0900

    a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,18 @@ class UsersController < ApplicationController
   def index2
   end
 
-  
+  def index3
+  end
+
+  def index4
+  end
+
+  def index5
+  end
+
+  def index6
+  end
+
+  def index7
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get 'users/index'
   get 'users/index2'
   get 'users/index3'
+  get 'users/index4'
+  get 'users/index5'
+  get 'users/index6'
+  get 'users/index7'
   root "exam#index"
   
   resources :items, only: [:new]
