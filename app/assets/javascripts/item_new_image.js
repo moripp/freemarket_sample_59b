a251710b14cf88751fe7017e7b6d935d2b41881f
moripp <msa.m16a.i212z4a@gmail.com>
2020-01-15 14:43:50 +0900

    create function

--- app/assets/javascripts/item_new_image.js
+++ app/assets/javascripts/item_new_image.js
@@ -2,3 +2,12 @@ $(function() {
   const insert_area = $('.image-upload-box'); 
   var n = 0;
 
+  // 【関数】　Blob URL Schemeを作成する（サムネイルを表示させるURLのようなもの）
+  function createBroburl(document) {
+    const file_list = document.files; // ファイルリストを取得
+    if(!file_list) return;
+    const file = file_list[0]; // 0番目のfileオブジェクトを取得
+    if(!file) return;
+    var brob_url = window.URL.createObjectURL(file); // Blob URL Schemeを生成
+    return brob_url;
+  }
