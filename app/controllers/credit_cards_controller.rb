0172ce2a6485e89c3e9b1175503af0c806de878b
Pirori3182 <pomuo.0331@gmail.com>
2020-01-19 17:03:19 +0900

    edit controller
    new,index,createアクションの中の記述の変更を行いました。

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -2,19 +2,20 @@ class CreditCardsController < ApplicationController
   require "payjp"
   # before_action :set_card
 
-  def new # カードの登録画面。送信ボタンを押すとcreateアクションへ。
-    card = CreditCard.where(user: current_user).first
-    # redirect_to action: :index if CreditCard.present?
-  end
-
   def index
+    card = current_user.credit_card
+    if card.blank?
+      render :new
+    else
+      render :create
+    end
   end
 
-  def create
+  def new
     Payjp.api_key = 'sk_test_09088df6ac5f5a56b2df8d37'
 
     if params['payjp-token'].blank?
-      # トークンがブランクなら、newアクションに飛ばす。
+      # paramsの中にjsで作った'payjpTokenが存在するか確かめる。トークンがブランクなら、newアクションに飛ばす。
       redirect_to action: "new"
     else
       # トークンが正常に発行されていたら、顧客情報をPAY.JPに登録する
@@ -22,7 +23,7 @@ class CreditCardsController < ApplicationController
         email: current_user.email,
         card: params['payjp-token'],
       )
-      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
         redirect_to action: "index"
       else
@@ -30,6 +31,17 @@ class CreditCardsController < ApplicationController
       end
     end
   end
+
+  def create
+    card = current_user.credit_card
+    if card.blank?
+      redirect_to action: "new" 
+    else
+      Payjp.api_key = "sk_test_09088df6ac5f5a56b2df8d37"
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @customer_card = customer.cards.retrieve(card.card_id)
+    end
+end
 
   # private
 
