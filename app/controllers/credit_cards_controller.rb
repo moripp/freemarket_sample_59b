0d07e2c692f88ce9ee297d1f6d4e3554fdc69768
Pirori3182 <pomuo.0331@gmail.com>
2020-01-21 21:05:30 +0900

    actionを一斉に変えた
    actionの役割を変えました

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -3,45 +3,40 @@ class CreditCardsController < ApplicationController
   # before_action :set_card
 
   def index
+    # Payjp.api_key = "sk_test_09088df6ac5f5a56b2df8d37"
     card = current_user.credit_card
     if card.blank?
-      render :new
+      redirect_to action: "new"
     else
-      render :create
+      @card = CreditCard.where(user_id: current_user.id).first
+      # customer = Payjp::Customer.retrieve(card.customer_id)
+      # @customer_card = customer.cards.retrieve(card.card_id)
     end
   end
 
   def new
-    Payjp.api_key = 'sk_test_09088df6ac5f5a56b2df8d37'
-
-    if params['payjp-token'].blank?
-      # paramsの中にjsで作った'payjpTokenが存在するか確かめる。トークンがブランクなら、newアクションに飛ばす。
-      redirect_to action: "new"
-    else
-      # トークンが正常に発行されていたら、顧客情報をPAY.JPに登録する
-      customer = Payjp::Customer.create(
-        email: current_user.email,
-        card: params['payjp-token'],
-      )
-      @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-      if @card.save
-        redirect_to action: "index"
-      else
-        redirect_to action: "create"
-      end
-    end
+    
   end
 
   def create
-    card = current_user.credit_card
-    if card.blank?
-      redirect_to action: "new" 
+    # Payjp.api_key = 'sk_test_09088df6ac5f5a56b2df8d37'
+    # if params['payjp-token'].blank?
+    #   # paramsの中にjsで作った'payjpTokenが存在するか確かめる。トークンがブランクなら、newアクションに飛ばす。
+    #   redirect_to action: "new"
+    # else
+    #   # トークンが正常に発行されていたら、顧客情報をPAY.JPに登録する
+    #   customer = Payjp::Customer.create(
+    #     email: current_user.email,
+    #     card: params['payjp-token'],
+    #   )
+    @card = CreditCard.new(user_id: current_user.id, customer_id: 1, card_id: 1)
+    if @card.save
+      redirect_to action: "index"
     else
-      Payjp.api_key = "sk_test_09088df6ac5f5a56b2df8d37"
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @customer_card = customer.cards.retrieve(card.card_id)
+      redirect_to action: "new"
     end
-end
+    # end
+  end
 
   # private
 
