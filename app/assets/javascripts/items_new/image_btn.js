fe33dafe906a45abb32c7faa76c0688754aadfc6
moripp <msa.m16a.i212z4a@gmail.com>
2020-01-22 23:37:08 +0900

    Put together files and Change file name
    ファイルをまとめただけの変更
    詳細は下記の通り
    
    ・javascriptsディレクトリ内にitems_newディレクトリ作成
    ・items_newは出品ページのビューファイル名に合わせている
    ・同じページに複数のJSファイルが必要になってきたのでまとめている
    ・ファイル名も分かりやすく変更

--- app/assets/javascripts/items_new/image_btn.js
+++ app/assets/javascripts/items_new/image_btn.js
@@ -1,8 +1,6 @@
 $(function() {
-  const insert_area = $('.image-upload-box'); 
-  var n = 0;
 
-  // 【関数】　Blob URL Schemeを作成する（サムネイルを表示させるURLのようなもの）
+  // 【関数】Blob URL Schemeを作成する（サムネイルを表示させるURLのようなもの）
   function createBroburl(document) {
     const file_list = document.files; // ファイルリストを取得
     if(!file_list) return;
@@ -12,27 +10,28 @@ $(function() {
     return brob_url;
   }
 
-  // 【関数】　アップロードした画像を表示させる
+  // 【関数】アップロードした画像を表示させる
   function addSellImage(brob_url, n, insert_area) {
     const html = `
       <div class="sell-image-upload-container">
         <figure class="sell-image-upload-container__image-box">
           <img id="file-preview" src="${brob_url}" style="width:114px; height: 114px;">
         </figure>
-        <div class="sell-image-upload-container__btn" id="sell-image-${n}"></div>
+        <div class="sell-image-upload-container__btn" id="sell-image-${n}">
+        </div>
       </div>
     `;
     $(html).insertBefore(insert_area);
   }
 
-  // 【関数】　画像アップロード済みボタンのclass名を変更し、場所を移動
+  // 【関数】画像アップロード済みボタンのclass名を変更し、場所を移動
   function moveBtn(document, n) {
     $(document).removeClass().addClass("image-upload-btn-uploaded");
     const insert_area = $(`#sell-image-${n}`);
     $(document).appendTo(insert_area);
   }
 
-  // 【関数】　新しい画像アップロードボタンを追加
+  // 【関数】新しい画像アップロードボタンを追加
   function addNewInput(insert_area, n) {
     const new_input = $(`<input class="image-upload-btn"
                                 accept="image/*"
@@ -42,6 +41,9 @@ $(function() {
                                 >`);
     insert_area.prepend(new_input);
   }
+
+  const insert_area = $('.image-upload-box'); 
+  var n = 0;
 
   $(document).on('change', '.image-upload-btn',function(e) {
     const brob_url = createBroburl(this); // Brob URL Scheme 作成
