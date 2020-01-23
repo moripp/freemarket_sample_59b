e6ff4928a92d7008680c74ccc649a2950c419c4f
Pirori3182 <pomuo.0331@gmail.com>
2020-01-23 22:06:33 +0900

    createとindexにpayjpへの問い合わせ処理を書いた

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -3,12 +3,13 @@ class CreditCardsController < ApplicationController
   # before_action :set_card
 
   def index
-    # Payjp.api_key = "sk_test_09088df6ac5f5a56b2df8d37"
+    Payjp.api_key = "sk_test_09088df6ac5f5a56b2df8d37"
     @card = current_user.credit_card
     if @card
+      customer = Payjp::Customer.retrieve(@card.customer_id) 
+      @customer_card = customer.cards.retrieve(@card.card_id) 
+      # binding.pry
       render :index_created
-      # customer = Payjp::Customer.retrieve(card.customer_id)
-      # @customer_card = customer.cards.retrieve(card.card_id) 
     else
       render :index
     end
@@ -19,23 +20,22 @@ class CreditCardsController < ApplicationController
   end
 
   def create
-    # Payjp.api_key = 'sk_test_09088df6ac5f5a56b2df8d37'
-    # if params['payjp-token'].blank?
-    #   # paramsの中にjsで作った'payjpTokenが存在するか確かめる。トークンがブランクなら、newアクションに飛ばす。
-    #   redirect_to action: "new"
-    # else
-    #   # トークンが正常に発行されていたら、顧客情報をPAY.JPに登録する
-    #   customer = Payjp::Customer.create(
-    #     email: current_user.email,
-    #     card: params['payjp-token'],
-    #   )
-    @card = CreditCard.new(user_id: current_user.id, customer_id: 1, card_id: 1)
-    if @card.save
-      redirect_to action: "index"
-    else
+    Payjp.api_key = 'sk_test_09088df6ac5f5a56b2df8d37'
+    if params['payjp-token'].blank?
+      # paramsの中にjsで作った'payjpTokenが存在するか確かめる。トークンがブランクなら、newアクションに飛ばす。
       redirect_to action: "new"
+    else
+      # トークンが正常に発行されていたら、顧客情報をPAY.JPに登録する
+      customer = Payjp::Customer.create(
+        card: params['payjp-token']
+      )
+      @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.cards.data[0].id)
+      if @card.save
+        redirect_to action: "index"
+      else
+        redirect_to action: "new"
+      end
     end
-    # end
   end
 
   # private
