af6cc027596c728d35ab72769537849d40a036b1
Kaori Oka <kaorioka09jm@gmail.com>
2020-01-25 19:14:38 +0900

    devise controller def

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+  } 
+  devise_scope :user do
+    get "sign_up", :to => "users/registrations#new"
+    get "sign_in", :to => "users/sessions#new"
+  end
   root "home#index"
   resources :items, only: [:new, :create]
   resources :profiles,only: [:new,:index]
