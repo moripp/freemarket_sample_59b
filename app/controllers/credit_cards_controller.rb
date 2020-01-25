dcd767e46eef960d6de2b27e0baabd0ae84f752c
Pirori3182 <pomuo.0331@gmail.com>
2020-01-25 17:44:42 +0900

    ストロングパラメーターの実装とコメントアウト
    ・カード情報を登録する際のストロングパラメーターの実装
    ・コメントアウトで説明
    しました！

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,46 +1,63 @@
 class CreditCardsController < ApplicationController
   require "payjp"
-  # before_action :set_card
-
+  
   def index
+    # payjpテスト秘密鍵をセット
     Payjp.api_key = "sk_test_09088df6ac5f5a56b2df8d37"
+    # 今ログインしているユーザーのクレジットカードを@cardに代入
     @card = current_user.credit_card
+    # もしcardがある場合
     if @card
+      # @cardから顧客情報(customer_id)を取得し customerに代入
       customer = Payjp::Customer.retrieve(@card.customer_id) 
+      # customerからcard_idを取得し @customer_card に代入
       @customer_card = customer.cards.retrieve(@card.card_id) 
-      # binding.pry
+      # index_createdのviewファイルを表示させる
       render :index_created
+    # もしcardがない場合
     else
+      # indexのviewファイルを表示させる
       render :index
     end
   end
-
+  
+  # クレジットカード登録フォーム
   def new
     
   end
 
   def create
+    # payjpテスト秘密鍵をセット
     Payjp.api_key = 'sk_test_09088df6ac5f5a56b2df8d37'
+    # paramsの中にjsで作った'payjp-Tokenが存在するか確かめる。まだ存在しない場合、
     if params['payjp-token'].blank?
-      # paramsの中にjsで作った'payjpTokenが存在するか確かめる。トークンがブランクなら、newアクションに飛ばす。
+      # newアクションに飛ばす。
       redirect_to action: "new"
+    # payjp-tokenが存在する場合
     else
-      # トークンが正常に発行されていたら、顧客情報をPAY.JPに登録する
+      # 顧客情報をPAY.JPに作成しcustomerに代入。その時にcardというキーに対して、payjp-tokenのparamsのvalueを入れる
       customer = Payjp::Customer.create(
-        card: params['payjp-token']
+        # card: params['payjp-token']
+        card: payjp_token_params[params['payjp-token']]
       )
+      # 引数に代入されている情報を元にクレジットカードの新しいインスタンスを作成し、@cardに代入。
       @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.cards.data[0].id)
+      # もしカード情報がsaveできたら
       if @card.save
+        # indexアクションに飛ばす。
         redirect_to action: "index"
       else
+        # そうではない場合newアクションに飛ばす。
         redirect_to action: "new"
       end
     end
   end
 
-  # private
+  private
+  
+  # Strong Parameters
+  def payjp_token_params
+    params.require(:"payjp-token")
+  end
 
-  # def set_card
-  #   @card = CreditCard.where(user_id: current_user.id).first if CreditCard.where(user_id: current_user.id).present?
-  # end
 end
