68387b7c43b6518b2f96b52ad4548cc731f33394
Kaori Oka <kaorioka09jm@gmail.com>
2020-01-25 19:15:43 +0900

    edit sessions controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,9 +4,9 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/sign_in
   # def create
@@ -16,7 +16,7 @@ class Users::SessionsController < Devise::SessionsController
   # DELETE /resource/sign_out
   # def destroy
   #   super
-  # end
+  # end 
 
   # protected
 
