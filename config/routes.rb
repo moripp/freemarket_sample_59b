6e0e749b8704bf3eefb67273da675a8e6e233255
hama-chan-tech-camp <g13tomo@docomo.ne.jp>
2020-01-27 21:24:15 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
+root "exam#index"
   
-  root "exam#index"
-  
-  resources :items, only: [:new]
-  resources :users do
+resources :items, only: [:new]
+resources :users do
             
-    collection do
-      get 'index'
-      get 'index2'
-      get 'index3'
-      get 'index4'
-      get 'index5'
-      get 'index6' 
-      get 'index7' 
+collection do
+    get 'index'
+    get 'index2'
+    get 'index3'
+    get 'index4'
+    get 'index5'
+    get 'index6' 
+    get 'index7' 
     end
   end
 end
