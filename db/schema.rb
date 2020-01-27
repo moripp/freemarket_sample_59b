f248483e3283cbd7338efca913a0b5fe4080a568
Kaori Oka <kaorioka09jm@gmail.com>
2020-01-27 23:13:15 +0900

    $ rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_12_074909) do
+ActiveRecord::Schema.define(version: 2020_01_25_124234) do
 
   create_table "credit_cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_01_12_074909) do
     t.text "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
