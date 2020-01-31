a8f9a394d334712086d8af14ee5a494dd02b21af
hama-chan-tech-camp <g13tomo@docomo.ne.jp>
2020-01-31 21:17:20 +0900

    フアイルの名前を変えたので、ルートの命名も変更しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,8 @@
 Rails.application.routes.draw do
-root "exam#index"
-  
 
-resources :items, only: [:new]
-resources :users do
-
-
-  
-  
- 
- 
-
-  devise_for :users
+  devise_for :users  
   root "home#index"
+  resources :users, only: [:index]
   resources :sitemap, only: [:index]
   resources :mypages, only: [:index]
   resources :items, only: [:new, :create, :show]
@@ -21,19 +11,15 @@ resources :users do
   resources :credit_cards,only: [:index,:new,:create]
   resources :logout,only: [:index]
   # ウィザード形式の記述
-  resources :signup do
+  resources :signup, only: [:new] do
     collection do
+      get 'select'
       get 'input_user_info'
       get 'input_phone_number'
       get 'input_address'
       get 'input_payment' # 入力最終ステップ
       get 'completion_registration' # 登録完了後のページ
 
-     
-
-            
-
-    end
   end
  end
 end
