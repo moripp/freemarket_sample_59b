49b0c5bf0072083fbabc803330328cb5f4d87427
hama-chan-tech-camp <g13tomo@docomo.ne.jp>
2020-01-31 21:56:15 +0900

    x

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,21 +2,5 @@ class UsersController < ApplicationController
   def index
   end
 
-  def index2
-  end
-
-  def index3
-  end
-
-  def index4
-  end
-
-  def index5
-  end
-
-  def index6
-  end
-
-  def index7
-  end
+  
 end
