4b79b9af87bf5b2eb0d9aa5902cb20138d35c0cd
Kaori Oka <kaorioka09jm@gmail.com>
2020-02-01 20:39:03 +0900

    add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
+  has_one :profile
 
          validates :name_sei, presence: true
          validates :name_mei, presence: true
