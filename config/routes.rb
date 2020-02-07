74e3e40a417d9219231829b4ea7c714ede46f54d
moripp <msa.m16a.i212z4a@gmail.com>
2020-02-07 15:28:26 +0900

    コントローラーのusersをuserに名前変更。それにともないrouteや必要なpath変更。deviseでusersを使う必要があった為。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users  
   root "home#index"
-  resources :users, only: [:index]
+  resources :user, only: [:index]
   resources :sitemap, only: [:index]
   resources :mypages, only: [:index]
   resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
@@ -27,6 +27,6 @@ Rails.application.routes.draw do
       get 'input_payment' # 入力最終ステップ
       get 'completion_registration' # 登録完了後のページ
 
+    end
   end
- end
 end
