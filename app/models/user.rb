73e7d98c3b37eb7f711c81e66e51297d6630be34
moripp <msa.m16a.i212z4a@gmail.com>
2020-02-08 16:27:10 +0900

    モデル

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,23 @@ class User < ApplicationRecord
   validates :city, presence: true
   validates :address, presence: true
   validates :tel, presence: true
+
+  def self.from_omniauth(auth)
+    # 次の2行で3パターンの動きをしている
+    # ①SNS認証したことがあればアソシエーションでuser情報取得
+    # ②SNS認証したこと無ければ、emailアドレスで検索してuser情報取得
+    # ③SNS認証したことが無く、emailアドレス検索でもuser情報が見つからない場合、新しくuserのレコードを作成する準備
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create # 条件に合うレコード取得、なければcreate
+    user = sns.user || User.where(email: auth.info.email).first_or_initialize( # 条件に合うレコード取得、なければnew（まだ保存はしない）
+      # user新規登録formに名前とメールアドレスを渡す準備
+      name_sei: auth.info.last_name,
+      name_mei: auth.info.first_name,
+      email: auth.info.email
+    )
+    if user.persisted? # ②のようにSNS認証はしたこと無いが、user情報が存在する場合
+      sns.user = user
+      sns.save # SnsCredentialのuser_idを更新
+    end
+    { user: user, sns: sns }
+  end
 end
