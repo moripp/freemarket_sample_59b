82a1b3098a361639d8df3555e03fd3c671817bfd
kido44 <t.kidokido@ezweb.ne.jp>
2020-02-12 20:53:44 +0900

    商品表示

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,5 +1,5 @@
 class MypagesController < ApplicationController
   def index
-    @items = Item.includes(:images).order(id: "DESC").limit(10)
+    @items =Item.where(user_id: current_user.id).includes(:images).order(id: "DESC").limit(10)
   end
 end
