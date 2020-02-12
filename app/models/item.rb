db713449c3d87bc3da8beffe8c4c653005d9c12a
moripp <msa.m16a.i212z4a@gmail.com>
2020-02-12 14:54:27 +0900

    以前書いた記述がなぜか消えていたので再度記述。画像の登録を10枚以内にするバリデーション。

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
 
   enum status: {for_sale: 0, not_sale: 1}
 
-  validates :images, associated: true
+  validates :images, associated: true, length: { maximum: 10 }
   validates :name, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length: { maximum: 1000 }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
