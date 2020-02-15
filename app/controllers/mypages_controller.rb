a55b360a21bf2d463164dad053ec84ec2030e43b
kido44 <t.kidokido@ezweb.ne.jp>
2020-02-15 20:56:16 +0900

    pagination

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,5 +1,5 @@
 class MypagesController < ApplicationController
   def index
-    @items =Item.where(user_id: current_user.id).includes(:images).order(id: "DESC").limit(10)
+    @items =Item.where(user_id: current_user.id).includes(:images).order(id: "DESC").page(params[:page]).per(5)
   end
 end
