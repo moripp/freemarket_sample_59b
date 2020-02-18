c98597293598f92ee04736abdedf4e773b85ee8f
moripp <msa.m16a.i212z4a@gmail.com>
2020-02-18 20:45:07 +0900

    ルーティング 追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
   }
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
+  
   root "home#index"
   resources :sitemap, only: [:index]
   resources :mypages, only: [:index]
