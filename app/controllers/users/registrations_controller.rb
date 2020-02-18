633e47d5305665870ed53e47511367f79cd4c2cf
moripp <msa.m16a.i212z4a@gmail.com>
2020-02-18 20:46:34 +0900

    sns認証に関する部分をprivate以下に移動

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,11 +1,15 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  # SNS認証してから新規登録をした場合は、パスワードを自動生成してcreateする
+  before_action :configure_params_if_authentication_sns, only: [:create]
+
   def create
+  private
+  def configure_params_if_authentication_sns
+    # SNS認証してから新規登録をした場合は、パスワードを自動生成してparamsに入れる
     if params[:sns_auth] == 'true'
       pass = Devise.friendly_token # パスワードを自動生成
       params[:user][:password] = pass
       params[:user][:password_confirmation] = pass
     end
-    super # create実行後、通常通りdeviseでのcreateアクションが動く
+  end
   end
 end
