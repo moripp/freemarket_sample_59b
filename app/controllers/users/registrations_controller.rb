05ef035e71db2dedd4c68a11284c658b580fdb73
moripp <msa.m16a.i212z4a@gmail.com>
2020-02-18 20:52:42 +0900

    新規登録の1ページ目、2ページ目の情報を全て保存する処理。

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -16,6 +16,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @address = @user.build_address
     render :new_address # セッションに保存したまま次の画面へ
   end
+
+  def create_address
+    @user = User.new(session["devise.regist_data"]["user"]) # セッションの情報からuserのインスタンス作成
+    @address = Address.new(address_params) # formの情報からaddressのインスタンス作成
+    unless @address.valid? # バリデーション確認
+      flash.now[:alert] = @address.errors.full_messages
+      render :new_address and return
+    end
+    @user.build_address(@address.attributes)
+    @user.save # userとaddressを同時に保存
+    sign_in(:user, @user)
+  end
+
   private
   def configure_params_if_authentication_sns
     # SNS認証してから新規登録をした場合は、パスワードを自動生成してparamsに入れる
@@ -25,5 +38,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
       params[:user][:password_confirmation] = pass
     end
   end
+
+  def address_params
+    params.require(:address)
+          .permit(:name_sei, :name_mei, :kana_sei, :kana_mei,
+                  :postal_code, :prefectures, :city, :address, :building,
+                  :tel)
   end
 end
