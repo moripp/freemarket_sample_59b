46fd7e7456647b659c582507e286172b22202950
moripp <msa.m16a.i212z4a@gmail.com>
2020-02-18 20:42:08 +0900

    DB修正
    user登録でウィザード形式を導入する為にDBを修正する必要がある。
    ①userテーブルから不要なカラム削除
    ②userテーブルにnicknameカラム追加
    ③addressテーブルを作成（住所などの情報がuserテーブルにまとまっていが、addressテーブルを作成することで分割した）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_07_071156) do
+ActiveRecord::Schema.define(version: 2020_02_18_075840) do
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name_sei"
+    t.string "name_mei"
+    t.string "kana_sei"
+    t.string "kana_mei"
+    t.string "postal_code"
+    t.string "prefectures"
+    t.string "city"
+    t.string "address"
+    t.string "building"
+    t.string "tel"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
 
   create_table "credit_cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -61,12 +78,6 @@ ActiveRecord::Schema.define(version: 2020_02_07_071156) do
     t.string "name_mei", default: "", null: false
     t.string "kana_mei", default: "", null: false
     t.date "birth_date", null: false
-    t.string "postal_code", default: "", null: false
-    t.string "prefectures", default: "", null: false
-    t.string "city", default: "", null: false
-    t.string "address", default: "", null: false
-    t.string "building", default: ""
-    t.string "tel", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -74,6 +85,7 @@ ActiveRecord::Schema.define(version: 2020_02_07_071156) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
