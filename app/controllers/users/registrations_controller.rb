99d5e4391375a2420dd85483ffe3af984d45179e
moripp <msa.m16a.i212z4a@gmail.com>
2020-02-18 20:51:35 +0900

    新規登録１ページ目のform情報処理をcreateに記述。DBに保存はしないでセッションに保存。

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,6 +6,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
   
   def create
+    @user = User.new(sign_up_params)
+    unless @user.valid? # バリデーション確認
+      flash.now[:alert] = @user.errors.full_messages
+      render :new and return
+    end
+    session["devise.regist_data"] = {user: @user.attributes} # セッションに格納
+    session["devise.regist_data"][:user]["password"] = params[:user][:password] # passwordだけ入らないのでやり直し
+    @address = @user.build_address
+    render :new_address # セッションに保存したまま次の画面へ
+  end
   private
   def configure_params_if_authentication_sns
     # SNS認証してから新規登録をした場合は、パスワードを自動生成してparamsに入れる
